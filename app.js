21f15dd2e5bb3e6faa7ddde98577430b29f77610
Sin0001 <bboyliljie@gmail.com>
2021-11-10 23:24:23 +0800

    Install express-session connect-flash

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
 const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'})) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({extended: true})) //使用bodyParser
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false })) // 載入 session
+app.use(flash()) // 載入flash
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
