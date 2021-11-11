90684843a1081390eaaf51fe1989256dc3fb71f5
Sin0001 <bboyliljie@gmail.com>
2021-11-11 15:43:01 +0800

    Add admin restaurant create form

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,10 +2,15 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 
 const adminController = {
+  // 後台首頁
   getRestaurants: (req, res) => {
     return Restaurant.findAll({ raw: true,}).then( restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
+  },
+  // 新增餐廳頁面
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
   }
 }
 module.exports = adminController
