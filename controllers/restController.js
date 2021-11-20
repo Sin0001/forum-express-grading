e2df38264098c55f26b6ee9ffe6af282d2be0752
Sin0001 <bboyliljie@gmail.com>
2021-11-20 15:10:10 +0800

    Update getFeeds to Promise.all

--- controllers/restController.js
+++ controllers/restController.js
@@ -71,24 +71,25 @@ const restController = {
 
   // 瀏覽最新動態、評論(前10)
   getFeeds: (req, res) => {
-    return Restaurant.findAll({
-      limit: 10,
-      raw: true,
-      nest: true,
-      order: [['createdAt', 'DESC']],
-      include: [Category]
-    }).then(restaurants => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [Category]
+      }),
       Comment.findAll({
         limit: 10,
         raw: true,
         nest: true,
         order: [['createdAt', 'DESC']],
         include: [User, Restaurant]
-      }).then(comments => {
-        return res.render('feeds', {
-          restaurants: restaurants,
-          comments: comments
-        })
+      })
+    ]).then(([restaurants, comments]) => {
+      return res.render('feeds', {
+        restaurants: restaurants,
+        comments: comments
       })
     })
   },
